16d924421fb4ed4b17dc7f52a2fa42f21ffb2c19
Pekka Enberg <penberg@iki.fi>
2020-12-26 21:33:01 +0200

    liblinux: use malloc() in network stack

--- usr/liblinux/src/internal/net.c
+++ usr/liblinux/src/internal/net.c
@@ -9,6 +9,7 @@
 #include <linux/if_ether.h>
 #include <netinet/in.h>
 
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
@@ -43,6 +44,12 @@ static bool net_input_one(struct io_buffer *iob)
 	}
 }
 
+static void free_iobuf(struct io_buffer *iob)
+{
+	// TODO: should we make this delayed free for better memory reuse?
+	free(iob);
+}
+
 bool net_input(struct packet_view *pk)
 {
 	bool ret = false;
@@ -51,21 +58,24 @@ bool net_input(struct packet_view *pk)
 
 	size_t len = pk->end - pk->start;
 
-	/* FIXME: allocate dynamically */
-	static struct io_buffer iob;
+	struct io_buffer *iob = malloc(sizeof(struct io_buffer));
+	assert(iob != NULL);
        
-	iob = (struct io_buffer) {
+	*iob = (struct io_buffer) {
 		.queue = __liblinux_eth_ioqueue,
 		.ref_count = 1,
 		.data = addr,
 		.len = len,
 		.offset = 0,
+#if 0 // FIXME
+		.complete = free_iobuf,
+#endif
 	};
 
-	while (io_buffer_remaining(&iob) > 0) {
-		ret |= net_input_one(&iob);
+	while (io_buffer_remaining(iob) > 0) {
+		ret |= net_input_one(iob);
 	}
-	io_buffer_put(&iob);
+	io_buffer_put(iob);
 
 	return ret;
 }
